8bd0249f900cd716c93d2c6039670318dad2fa28
i4210577 <90830668+i4210577@users.noreply.github.com>
2025-03-13 08:57:24 +0000

    Update diy-part2.sh

--- redmi-ax6000/hanwckf/diy/diy-part2.sh
+++ redmi-ax6000/hanwckf/diy/diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
