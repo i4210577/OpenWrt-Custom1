ad20f3e3d3ace40a9ac3835221a1467e13f66026
i4210577 <90830668+i4210577@users.noreply.github.com>
2025-03-16 09:36:32 +0000

    Update diy-part2.sh

--- redmi-ax6000/hanwckf-110/diy/diy-part2.sh
+++ redmi-ax6000/hanwckf-110/diy/diy-part2.sh
@@ -20,45 +20,6 @@ sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_genera
 # ttyd自动登录
 #sed -i "s?/bin/login?/usr/libexec/login.sh?g" feeds/packages/utils/ttyd/files/ttyd.config
 
-# Theme
-#git clone https://github.com/SAENE/luci-theme-design package/luci-theme-design
-
-# luci-app-adguardhome
-#git clone https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
-
-# 安装 mosdns
-#rm -rf feeds/packages/lang/golang
-#rm -rf feeds/packages/net/mosdns
-#rm -rf package/feeds/packages/mosdns
-#rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf package/feeds/packages/v2ray-geodata
-#git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-# 获取隔空播放luci-app-airconnect
-#git clone https://github.com/sbwml/luci-app-airconnect package/airconnect
-
-# OpenClash
-#git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
-#rm -rf feeds/luci/applications/luci-app-openclash
-#mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
-
-# luci-app-tailscale
-#sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
-#git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
-
-# lucky
-#git clone https://github.com/sirpdboy/luci-app-lucky package/lucky
-
-# 一键配置拨号
-# git clone https://github.com/caiweill/luci-app-netwizard package/luci-app-netwizard
-
-# 进阶设置
-#git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
-
-# 定时设置
-#git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 
 
 # 更改菜单名字
