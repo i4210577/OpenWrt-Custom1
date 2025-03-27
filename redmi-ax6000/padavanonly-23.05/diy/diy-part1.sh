75131c235f33826cac9c1ffe37bfe421ffc4e873
i4210577 <90830668+i4210577@users.noreply.github.com>
2025-03-27 07:04:18 +0000

    Update diy-part1.sh

--- redmi-ax6000/padavanonly-23.05/diy/diy-part1.sh
+++ redmi-ax6000/padavanonly-23.05/diy/diy-part1.sh
@@ -18,19 +18,28 @@
 #sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
 sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
 
+# mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
+# OpenClash
 #git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+# adguardhome
 #git clone https://github.com/SuperArilo/luci-app-adguardhome.git package/luci-app-adguardhome
+# 网络唤醒
 git clone https://github.com/Firsgith/luci-app-wolplus.git package/luci-app-wolplus
+# lucky
 git clone https://github.com/sirpdboy/luci-app-lucky package/lucky
+# 进阶设置
 git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
 #git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
+# vnt组网
 git clone https://github.com/lmq8267/luci-app-vnt.git package/vnt
-#git clone https://github.com/caiweill/luci-app-netwizard package/luci-app-netwizard
+# 定时设置
 #git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 # 安装tailscale组网
 sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
 git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+# Theme
+#git clone https://github.com/SAENE/luci-theme-design package/luci-theme-design
 
 # 安装 OpenClash
 git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
@@ -68,26 +77,26 @@ chmod +x "$CORE_FILE"
 echo "Mihomo 内核已成功下载并配置到 $CORE_FILE"
 
 # 下载 OpenClash GeoIP 数据库
-GEOIP_FILE="feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoIP.dat"
-curl -sL --retry 3 --retry-delay 5 -m 30 "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" -o "/tmp/GeoIP.dat"
+#GEOIP_FILE="feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoIP.dat"
+#curl -sL --retry 3 --retry-delay 5 -m 30 "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" -o "/tmp/GeoIP.dat"
 
-if [ -s "/tmp/GeoIP.dat" ]; then
-    mv -f "/tmp/GeoIP.dat" "$GEOIP_FILE"
-    echo "GeoIP 数据库更新成功！"
-else
-    echo "GeoIP 数据库下载失败！"
-fi
+#if [ -s "/tmp/GeoIP.dat" ]; then
+#    mv -f "/tmp/GeoIP.dat" "$GEOIP_FILE"
+#    echo "GeoIP 数据库更新成功！"
+#else
+#    echo "GeoIP 数据库下载失败！"
+#fi
 
 # 下载 OpenClash GeoSite 数据库
-GEOSITE_FILE="feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoSite.dat"
-curl -sL --retry 3 --retry-delay 5 -m 30 "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" -o "/tmp/GeoSite.dat"
+#GEOSITE_FILE="feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoSite.dat"
+#curl -sL --retry 3 --retry-delay 5 -m 30 "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" -o "/tmp/GeoSite.dat"
 
-if [ -s "/tmp/GeoSite.dat" ]; then
-    mv -f "/tmp/GeoSite.dat" "$GEOSITE_FILE"
-    echo "GeoSite 数据库更新成功！"
-else
-    echo "GeoSite 数据库下载失败！"
-fi
+#if [ -s "/tmp/GeoSite.dat" ]; then
+#    mv -f "/tmp/GeoSite.dat" "$GEOSITE_FILE"
+#    echo "GeoSite 数据库更新成功！"
+#else
+#    echo "GeoSite 数据库下载失败！"
+#fi
 
 #修复Coremark编译失败
 sed -i 's/mkdir/mkdir -p/g' feeds/packages/utils/coremark/Makefile
